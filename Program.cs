d7ef03753a342ffc544e5a46a0e9aea315de8330
Sean Paul Erasan <sean.p@cloudengage.com>
2021-11-20 23:05:13 +0800

    added route for the home page

--- Program.cs
+++ Program.cs
@@ -22,6 +22,9 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Login}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=Index}");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}");
 
 app.Run();
