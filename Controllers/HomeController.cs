dd65827f0414586bf17fe1fb8124cd85f01e4f6d
Sean Paul Erasan <sean.p@cloudengage.com>
2021-11-20 23:26:02 +0800

    added home page controller and view

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using exercise.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace exercise.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
