89c144da3fa8958fcc6312e76dec2dfdb1d28873
Sean Paul Erasan <sean.p@cloudengage.com>
2021-11-20 23:02:31 +0800

    added the login controller integrated a vue component

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using exercise.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace exercise.Controllers
+{
+    public class LoginController : Controller
+    {
+        public LoginController()
+        {
+            //todo
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
