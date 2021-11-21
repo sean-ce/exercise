f07f586508ff2f2aad5c497d40f778f754d30e67
Sean Paul Erasan <sean.p@cloudengage.com>
2021-11-21 15:55:12 +0800

    complete implementation of login controller

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,19 +1,38 @@
-﻿using exercise.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net.Http;
+using System.Text.Json;
 
 namespace exercise.Controllers
 {
     public class LoginController : Controller
     {
-        public LoginController()
+        
+        public async Task<IActionResult> IndexAsync()
         {
-            //todo
+
+            if (Request.Method == "POST") {
+                Debug.WriteLine("Authenticate the credentials");
+                await Authenticate();
+            }
+            
+            return View();
         }
 
-        public IActionResult Index()
-        {
-            return View();
+        public async Task Authenticate() {
+            HttpClient client = new ();
+            var user = new { 
+                username = Request.Form["username"].ToString(),
+                password = Request.Form["password"].ToString()
+            };
+
+            HttpResponseMessage? response = await client.PostAsJsonAsync("https://netzwelt-devtest.azurewebsites.net/Account/SignIn", user);
+
+            if (response.IsSuccessStatusCode) {
+                Response.Redirect("/home/index");
+            }
+
+            ViewBag.AuthMessage = "Invalid username or password";
         }
     }
 }
