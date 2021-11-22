2e7885d506d1775ef6456a2eb80956d624048e0a
Sean Paul Erasan <sean.p@cloudengage.com>
2021-11-22 11:44:49 +0800

    -added the cookie -doing some cleanup -added comments

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,18 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using System.Net.Http;
-using System.Text.Json;
 
 namespace exercise.Controllers
 {
     public class LoginController : Controller
     {
-        
         public async Task<IActionResult> IndexAsync()
         {
 
             if (Request.Method == "POST") {
-                Debug.WriteLine("Authenticate the credentials");
+                /*
+                 * form submission, validate the credentials
+                 */
                 await Authenticate();
             }
             
@@ -21,17 +19,36 @@ namespace exercise.Controllers
 
         public async Task Authenticate() {
             HttpClient client = new ();
+            /*
+             * set the payload from the form submitted
+             */
             var user = new { 
                 username = Request.Form["username"].ToString(),
                 password = Request.Form["password"].ToString()
             };
 
+            /*
+             * authentication
+             */
             HttpResponseMessage? response = await client.PostAsJsonAsync("https://netzwelt-devtest.azurewebsites.net/Account/SignIn", user);
 
+            /*
+             * we only want to redirect to /, /home, /home/index if successful authentication
+             */
             if (response.IsSuccessStatusCode) {
+                /*
+                 * we set the cookie to expire within 1 seconds
+                 * more than enough time for the redirect to happen
+                 */
+                CookieOptions cookieOptions = new CookieOptions();
+                cookieOptions.Expires = new DateTimeOffset(DateTime.Now.AddSeconds(1));
+                Response.Cookies.Append("isAuthenticated", "true", cookieOptions);
                 Response.Redirect("/home/index");
             }
 
+            /*
+             * no redirect will show the authentication error message
+             */
             ViewBag.AuthMessage = "Invalid username or password";
         }
     }
