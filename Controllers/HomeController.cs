517418cd323169085746a47c835107d830c432ed
Sean Paul Erasan <sean.p@cloudengage.com>
2021-11-22 11:48:18 +0800

    home controller update -checking cookie and redirect if we don't see isAuthenticated=true

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,4 @@
-﻿using exercise.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace exercise.Controllers
 {
@@ -8,11 +6,35 @@ namespace exercise.Controllers
     {
         public HomeController()
         {
-
         }
 
         public IActionResult Index()
         {
+            /*
+             * initialize to redirect to the login page 
+             */
+            Boolean redirect = true;
+            foreach (KeyValuePair<string, string> cookie in Request.Cookies) {
+                /*
+                 * reset the redirect to false if we see the cookie with the corresponding
+                 */
+                if (cookie.Key == "isAuthenticated" && cookie.Value == "true") { 
+                    redirect = false;
+                    /*
+                     * we make sure we clear the cookie 
+                     * refreshing the page will redirect back to login
+                     */
+                    Response.Cookies.Delete(cookie.Key);
+                    break;
+                }
+            }
+
+            if (redirect) {
+                Response.Redirect("/account/login");
+            }
+
+
+
             return View();
         }
     }
